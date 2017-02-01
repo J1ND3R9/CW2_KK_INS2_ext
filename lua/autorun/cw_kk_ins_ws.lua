de34bf6a648bc96649f925cc1e1bd6383022d91a
Ľubor Slušný <lubor.slusny@gmail.com>
2017-02-01 20:22:23 +0100

    moved additional doi content from base pack here

--- lua/autorun/cw_kk_ins_ws.lua
+++ lua/autorun/cw_kk_ins_ws.lua
@@ -2,4 +2,4 @@ AddCSLuaFile()
 
 CustomizableWeaponry_KK = CustomizableWeaponry_KK or {}
 CustomizableWeaponry_KK.ins2 = CustomizableWeaponry_KK.ins2 or {}
-CustomizableWeaponry_KK.ins2.ws = 9 // update WS_PACK_REVISION in cw_kk_ins.lua
+CustomizableWeaponry_KK.ins2.ws = 10 // update WS_PACK_REVISION in cw_kk_ins.lua
